4fdedd74bc425eaac1be88f03d638d17c93c590c
nidishbonthapally <nidishbonthapally1227@gmail.com>
2026-05-20 13:37:43 +0530

    day-2 constructors

--- src/Bank.java
+++ src/Bank.java
@@ -1,23 +1,23 @@
 public class Bank {
         String userName;
         double balance;
-        long accountNumber;
+        long accountNo;
 
         Bank(String userName, double balance, long accountNumber){
             this.userName = userName;
             this.balance = balance;
-            this.accountNumber = accountNumber;
+            this.accountNo= accountNumber;
         }
 
         void deposit(int amount){
             balance = balance + amount;
-            System.out.println(userName + " with " + accountNumber + " has deposited the amount " + amount + " into the account with account number " + accountNumber);
+            System.out.println(userName + " with " + accountNo + " has deposited the amount " + amount + " into the account with account number " + accountNo);
             System.out.println("Balance: " + balance);
         }
 
         void withdraw(int amount){
             balance = balance-amount;
-            System.out.println(userName + " with " + accountNumber + " has withdrawn the amount " + amount + " from the account with account number " + accountNumber);
+            System.out.println(userName + " with " + accountNo + " has withdrawn the amount " + amount + " from the account with account number " + accountNo);
             System.out.println("Balance: " + balance);
         }
 }
--- src/BatteryLogic.java
+++ src/BatteryLogic.java
@@ -1,17 +1,13 @@
 public class BatteryLogic {
     public static void main(String[] args) {
-        Phone p1 = new Phone();
-        p1.brand = "Samsung";
-        p1.version = 16.0;
-        p1.battery_level = 80;
+        Phone p1 = new Phone("Samsung",16, 50);
+        Phone p2 = new Phone("IPhone", 22, 30);
 
-        Phone p2 = new Phone();
-        p2.brand = "IPhone";
-        p2.version = 20.2;
-        p2.battery_level = 20;
 
         p1.charge();
         p2.use();
 
+        p1.use();
+        p2.charge();
     }
 }
