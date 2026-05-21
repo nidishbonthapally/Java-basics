2925e82657903b1df7f9aa94d83e14e4331ab98b
nidishbonthapally <nidishbonthapally1227@gmail.com>
2026-05-21 15:04:56 +0530

    day-3 Encapsulation

--- src/Bank.java
+++ src/Bank.java
@@ -1,12 +1,48 @@
 public class Bank {
-        String userName;
-        double balance;
-        long accountNo;
+        private String userName;
+        private double balance;
+        private long accountNo;
 
-        Bank(String userName, double balance, long accountNumber){
+        public Bank(String userName, double balance, long accountNumber){
             this.userName = userName;
             this.balance = balance;
             this.accountNo= accountNumber;
+        }
+
+        public String getUserName(){
+            return userName;
+        }
+
+        public double getBalance() {
+        return balance;
+        }
+
+        public long getAccountNo() {
+        return accountNo;
+        }
+
+        public void setUserName(String userName) {
+            if(userName.length()> 3) {
+                this.userName = userName;
+            }else {
+                System.out.println("Username should have more than 3 characters");
+            }
+        }
+
+        public void setAccountNo(long accountNo) {
+            if (accountNo>999999999) {
+                this.accountNo = accountNo;
+            }else {
+                System.out.println("Invalid account number");
+            }
+        }
+
+        public void setBalance(double balance) {
+            if (balance>=0.0) {
+                this.balance = balance;
+            }else {
+                System.out.println("balance cannot be less than 0.0");
+            }
         }
 
         void deposit(int amount){
@@ -16,8 +52,15 @@ public class Bank {
         }
 
         void withdraw(int amount){
-            balance = balance-amount;
-            System.out.println(userName + " with " + accountNo + " has withdrawn the amount " + amount + " from the account with account number " + accountNo);
-            System.out.println("Balance: " + balance);
+            if (balance>amount) {
+                balance = balance - amount;
+                System.out.println(userName + " with " + accountNo + " has withdrawn the amount " + amount + " from the account with account number " + accountNo);
+                System.out.println("Balance: " + balance);
+            }
+            else {
+                System.out.println("Insufficient balance");
+                System.out.println("Balance: " + balance);
+            }
+
         }
-}
+    }
--- src/UserBank.java
+++ src/UserBank.java
@@ -1,6 +1,6 @@
 public class UserBank {
     public static void main(String[] args) {
-        Bank u1 = new Bank("John Doe", 100000.500, 1015237892);
+        Bank u1 = new Bank("John Doe", 100000.55, 1015237892);
         Bank u2 = new Bank("John Boe", 50000, 1239638527);
 
         u1.withdraw(20000);
@@ -8,5 +8,10 @@ public class UserBank {
 
         u2.withdraw(10000);
         u2.deposit(5000);
+
+        System.out.println(u1.getBalance());
+        System.out.println(u1.getAccountNo());
+        System.out.println(u1.getUserName());
+        u2.withdraw(50000000);
     }
 }
